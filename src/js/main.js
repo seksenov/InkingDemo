4ff176b545701a5c4e32a80ff6d7646b3812a898
Kiril Seksenov <seksenovk@gmail.com>
2016-02-09 09:49:19 -0800

    adding STL file

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,32 @@
-var wrapper = document.getElementById("signature-pad"),
+var signaturePad;
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    console.log("dom loaded");
+
+    var wrapper = document.getElementById("signature-pad"),
     clearButton = wrapper.querySelector("[data-action=clear]"),
     saveButton = wrapper.querySelector("[data-action=save]"),
-    canvas = wrapper.querySelector("canvas"),
-    signaturePad;
+    canvas = wrapper.querySelector("canvas");
+
+    clearButton.addEventListener("click", function (event) {
+        signaturePad.clear();
+    });
+
+    saveButton.addEventListener("click", function (event) {
+        if (signaturePad.isEmpty()) {
+            alert("Please provide signature first.");
+        } else {
+            window.open(signaturePad.toDataURL());
+        }
+    });
+
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+
+    signaturePad = new SignaturePad(canvas);
+}
 
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
@@ -11,7 +35,8 @@ function resizeCanvas() {
     // When zoomed out to less than 100%, for some very strange reason,
     // some browsers report devicePixelRatio as less than 1
     // and only part of the canvas is cleared then.
-    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1),
+    canvas = document.getElementById("draw-surface");
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
     canvas.getContext("2d").scale(ratio, ratio);
@@ -20,16 +45,5 @@ function resizeCanvas() {
 window.onresize = resizeCanvas;
 resizeCanvas();
 
-signaturePad = new SignaturePad(canvas);
 
-clearButton.addEventListener("click", function (event) {
-    signaturePad.clear();
-});
 
-saveButton.addEventListener("click", function (event) {
-    if (signaturePad.isEmpty()) {
-        alert("Please provide signature first.");
-    } else {
-        window.open(signaturePad.toDataURL());
-    }
-});
